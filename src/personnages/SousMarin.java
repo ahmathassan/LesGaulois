e1cfada873bb1550f58d53c6bfa901b219f22252
ahmathassan <welleymi@gmail.com>
2023-03-07 16:27:49 +0100

    fin de tp1

---
+++ src/personnages/SousMarin.java
@@ -0,0 +1,12 @@
+package personnages;
+	private string nom;
+	private int force;
+	private int effetPotion = 1;
+	
+	public SousMarin(string nom, int force) {
+		super();
+		this.nom = nom;
+		this.force = force;
+	}
+
+}
